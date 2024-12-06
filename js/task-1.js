6b68ddaab533b7a65ccde716fc7e7bae320aca63
yLevytsk <Lupin773311asd@gmail.com>
2024-12-06 09:30:05 +0000

    redi

---
+++ js/task-1.js
@@ -0,0 +1,16 @@
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  // Рассчитываем общую стоимость заказа
+  const totalPrice = quantity * pricePerDroid;
+
+  // Проверяем, достаточно ли у клиента средств
+  if (totalPrice > customerCredits) {
+    return "Insufficient funds!";
+  } else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
+}
+
+// Проверка работы функции
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(10, 500, 5000));  // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(7, 1000, 5000));  // "Insufficient funds!"
